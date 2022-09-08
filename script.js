4ad22567e85595e831dbe98064547c9714cea9b8
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2022-09-08 22:33:43 +0500

    add Loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random Quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -24,11 +38,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
